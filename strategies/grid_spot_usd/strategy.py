27a4e4c5ca4b3c5a7b127e86f1b2cb56ced92bc0
TristanLeGoff <tristan.le_goff@edu.devinci.fr>
2022-03-31 10:26:48 +0200

    first commit

---
+++ strategies/grid_spot_usd/strategy.py
@@ -0,0 +1,109 @@
+import sys
+sys.path.append('live_tools/utilities')
+import ccxt
+import pandas as pd
+import ta
+from spot_ftx import SpotFtx
+from datetime import datetime
+import time
+import json
+f = open('./live_tools/strategies/grid_spot_usd/last_data.json',)
+last_data = json.load(f)
+f.close()
+
+f = open('./live_tools/secret.json',)
+secret = json.load(f)
+f.close()
+
+def custom_grid(first_price, last_order_down = 0.5, last_order_up = 1, down_grid_len=10, up_grid_len=10):
+    down_pct_unity = last_order_down / down_grid_len
+    up_pct_unity = last_order_up / up_grid_len
+
+    grid_sell = []
+    grid_buy = []
+
+    for i in range(down_grid_len):
+        grid_buy.append(first_price - first_price*down_pct_unity*(i+1))
+
+    for i in range(up_grid_len):
+        grid_sell.append(first_price + first_price*up_pct_unity*(i+1))
+
+    return grid_buy, grid_sell
+
+now = datetime.now()
+print(now.strftime("%d-%m %H:%M:%S"))
+
+account_to_select = "account1"
+ftx = SpotFtx(
+        apiKey=secret[account_to_select]["apiKey"],
+        secret=secret[account_to_select]["secret"],
+        subAccountName=secret[account_to_select]["subAccountName"]
+    )
+
+symbol = 'BTC/USD'
+coin1 = "BTC"
+coin2 = "USD"
+total_orders = 10
+
+current_price = ftx.get_bid_ask_price(symbol)["bid"]
+
+orders_list = []
+for order in ftx.get_open_order():
+    orders_list.append(order["info"])
+
+df_order = pd.DataFrame(orders_list)
+if df_order.empty == False:
+    df_order["price"] = pd.to_numeric(df_order["price"])
+    df_order["size"] = pd.to_numeric(df_order["size"])
+# print(df_order)
+
+coin1_balance = ftx.get_detail_balance_of_one_coin(coin1)["free"]
+coin2_balance = ftx.get_detail_balance_of_one_coin(coin2)["free"]
+# print(coin1_balance, coin2_balance)
+
+if df_order.empty or len(df_order.loc[df_order["side"] == "buy"]) == 0 or len(df_order.loc[df_order["side"] == "sell"]) == 0:
+    print("create new grid")
+    grid_buy, grid_sell = custom_grid(50000, last_order_down = 0.6, last_order_up = 3, down_grid_len=5, up_grid_len=5)
+    for buy in grid_buy:
+        # print(buy,(coin2_balance/buy)/len(grid_buy))
+        ftx.place_limit_order(symbol=symbol, side='buy', amount=(coin2_balance/buy)/len(grid_buy), price=buy)
+
+    for sell in grid_sell:
+        # print(sell,coin1_balance/len(grid_sell))
+        ftx.place_limit_order(symbol=symbol, side='sell', amount=coin1_balance/len(grid_sell), price=sell)
+
+elif total_orders == len(df_order):
+    print("no new orders")
+
+else:
+    buy_order_to_create = last_data["number_of_sell_orders"] - len(df_order.loc[df_order["side"] == "sell"])
+    sell_order_to_create = last_data["number_of_buy_orders"] - len(df_order.loc[df_order["side"] == "buy"])
+    print("Create", buy_order_to_create, "new buy orders")
+    print("Create", sell_order_to_create, "new sell orders")
+    last_buy_order = df_order.loc[df_order["side"] == "buy"]["price"].max()
+    last_sell_order = df_order.loc[df_order["side"] == "sell"]["price"].min()
+
+    diff_buy = (current_price - last_buy_order) / (buy_order_to_create + 1)
+    diff_sell = (last_sell_order - current_price) / (sell_order_to_create + 1)
+
+    for i in range(buy_order_to_create):
+        # print("buy",current_price - diff_buy*(i+1))
+        ftx.place_limit_order(symbol=symbol, side='buy', amount=(coin2_balance/current_price)/buy_order_to_create, price=current_price - diff_buy*(i+1))
+    for i in range(sell_order_to_create):
+        # print("sell",current_price + diff_sell*(i+1))
+        ftx.place_limit_order(symbol=symbol, side='sell', amount=coin1_balance/sell_order_to_create, price=current_price + diff_sell*(i+1))
+
+orders_list = []
+for order in ftx.get_open_order():
+    orders_list.append(order["info"])
+
+df_order = pd.DataFrame(orders_list)
+if df_order.empty == False:
+    last_data["number_of_buy_orders"] = len(df_order.loc[df_order["side"] == "buy"])
+    last_data["number_of_sell_orders"] = len(df_order.loc[df_order["side"] == "sell"])
+else:
+    last_data["number_of_buy_orders"] = 0
+    last_data["number_of_sell_orders"] = 0
+
+with open('./live_tools/strategies/grid_spot_usd/last_data.json', 'w') as outfile:
+    json.dump(last_data, outfile)
---
+++ utilities/spot_ftx.py
@@ -0,0 +1,243 @@
+import ccxt
+import pandas as pd
+import time
+
+class SpotFtx():
+    def __init__(self, apiKey=None, secret=None, subAccountName=None):
+        ftxAuthObject = {
+            "apiKey": apiKey,
+            "secret": secret,
+            'headers': {
+                'FTX-SUBACCOUNT': subAccountName
+            }
+        }
+        if ftxAuthObject['secret'] == None:
+            self._auth = False
+            self._session = ccxt.ftx()
+        else:
+            self._auth = True
+            self._session = ccxt.ftx(ftxAuthObject)
+        self.market = self._session.load_markets()
+
+    def authentication_required(fn):
+        """Annotation for methods that require auth."""
+        def wrapped(self, *args, **kwargs):
+            if not self._auth:
+                print("You must be authenticated to use this method", fn)
+                exit()
+            else:
+                return fn(self, *args, **kwargs)
+        return wrapped
+
+    def get_historical_since(self, symbol, timeframe, startDate):
+        try:
+            tempData = self._session.fetch_ohlcv(symbol, timeframe, int(
+                time.time()*1000)-1209600000, limit=1000)
+            dtemp = pd.DataFrame(tempData)
+            timeInter = int(dtemp.iloc[-1][0] - dtemp.iloc[-2][0])
+        except:
+            return None
+
+        finished = False
+        start = False
+        allDf = []
+        startDate = self._session.parse8601(startDate)
+        while(start == False):
+            try:
+                tempData = self._session.fetch_ohlcv(
+                    symbol, timeframe, startDate, limit=1000)
+                dtemp = pd.DataFrame(tempData)
+                timeInter = int(dtemp.iloc[-1][0] - dtemp.iloc[-2][0])
+                nextTime = int(dtemp.iloc[-1][0] + timeInter)
+                allDf.append(dtemp)
+                start = True
+            except:
+                startDate = startDate + 1209600000*2
+
+        if dtemp.shape[0] < 1:
+            finished = True
+        while(finished == False):
+            try:
+                tempData = self._session.fetch_ohlcv(
+                    symbol, timeframe, nextTime, limit=1000)
+                dtemp = pd.DataFrame(tempData)
+                nextTime = int(dtemp.iloc[-1][0] + timeInter)
+                allDf.append(dtemp)
+                # print(dtemp.iloc[-1][0])
+                if dtemp.shape[0] < 1:
+                    finished = True
+            except:
+                finished = True
+        result = pd.concat(allDf, ignore_index=True, sort=False)
+        result = result.rename(
+            columns={0: 'timestamp', 1: 'open', 2: 'high', 3: 'low', 4: 'close', 5: 'volume'})
+        result = result.set_index(result['timestamp'])
+        result.index = pd.to_datetime(result.index, unit='ms')
+        del result['timestamp']
+        return result
+
+    def get_last_historical(self, symbol, timeframe, limit):
+        result = pd.DataFrame(data=self._session.fetch_ohlcv(
+            symbol, timeframe, None, limit=limit))
+        result = result.rename(
+            columns={0: 'timestamp', 1: 'open', 2: 'high', 3: 'low', 4: 'close', 5: 'volume'})
+        result = result.set_index(result['timestamp'])
+        result.index = pd.to_datetime(result.index, unit='ms')
+        del result['timestamp']
+        return result
+
+    def get_bid_ask_price(self, symbol):
+        try:
+            ticker = self._session.fetchTicker(symbol)
+        except BaseException as err:
+            print("An error occured", err)
+            exit()
+        return {"bid":ticker["bid"],"ask":ticker["ask"]}
+
+    def get_min_order_amount(self, symbol):
+        return self._session.markets_by_id[symbol]['limits']['amount']['min']
+
+    def convert_amount_to_precision(self, symbol, amount):
+        return self._session.amount_to_precision(symbol, amount)
+
+    def convert_price_to_precision(self, symbol, price):
+        return self._session.price_to_precision(symbol, price)
+
+    @authentication_required
+    def get_all_balance(self):
+        try:
+            allBalance = self._session.fetchBalance()
+        except BaseException as err:
+            print("An error occured", err)
+            exit()
+        return allBalance['total']
+
+    @authentication_required
+    def get_all_balance_in_usd(self):
+        try:
+            allBalance = self._session.fetchBalance()
+            allBalance = allBalance['total']
+            for coin in allBalance:
+                if coin != 'USD':
+                    try:
+                        allBalance[coin] = float(allBalance[coin]) * float(self.market[coin+'/USD']['info']['last'])
+                    except:
+                        pass
+                        print("Cannot get price of",coin+'/USD')
+        except BaseException as err:
+            print("An error occured", err)
+            exit()
+        return allBalance
+
+    @authentication_required
+    def get_balance_of_one_coin(self, coin):
+        try:
+            allBalance = self._session.fetchBalance()
+        except BaseException as err:
+            print("An error occured", err)
+            exit()
+        try:
+            return allBalance['total'][coin]
+        except:
+            return 0
+
+    @authentication_required
+    def get_detail_balance_of_one_coin(self, coin):
+        try:
+            allBalance = self._session.fetchBalance()
+        except BaseException as err:
+            print("An error occured", err)
+            exit()
+        try:
+            return allBalance[coin]
+        except:
+            return 0
+
+    @authentication_required
+    def place_market_order(self, symbol, side, amount):
+        try:
+            return self._session.createOrder(
+                symbol, 
+                'market', 
+                side, 
+                self.convert_amount_to_precision(symbol, amount),
+                None
+            )
+        except BaseException as err:
+            print("An error occured", err)
+            exit()
+
+    @authentication_required
+    def place_limit_order(self, symbol, side, amount, price):
+        try:
+            return self._session.createOrder(
+                symbol, 
+                'limit', 
+                side, 
+                self.convert_amount_to_precision(symbol, amount), 
+                self.convert_price_to_precision(symbol, price)
+                )
+        except BaseException as err:
+            print("An error occured", err)
+            exit()
+
+    @authentication_required
+    def place_market_stop_loss(self, symbol, amount, price):
+        params = {
+        'stopPrice': self.convert_price_to_precision(symbol, price),  # your stop price
+        }
+        try:
+            return self._session.createOrder(
+                symbol, 
+                'stop', 
+                'sell', 
+                self.convert_amount_to_precision(symbol, amount), 
+                None,
+                params
+                )
+        except BaseException as err:
+            print("An error occured", err)
+            exit()
+
+    @authentication_required
+    def cancel_all_open_order(self, symbol):
+        try:
+            return self._session.cancel_all_orders(symbol)
+        except BaseException as err:
+            print("An error occured", err)
+            exit()
+
+    @authentication_required
+    def cancel_order_by_id(self, id):
+        try:
+            return self._session.cancel_order(id)
+        except BaseException as err:
+            print("An error occured", err)
+            exit()
+
+    @authentication_required
+    def get_open_order(self):
+        try:
+            return self._session.fetchOpenOrders()
+        except BaseException as err:
+            print("An error occured", err)
+            exit()
+
+    @authentication_required
+    def get_open_stop_order(self):
+        params = {
+            'type':'stop'
+        }
+        try:
+            return self._session.fetchOpenOrders(None,None,None,params)
+        except BaseException as err:
+            print("An error occured", err)
+            exit()
+
+    @authentication_required
+    def get_my_trades(self, symbol=None, since=None, limit=1):
+        try:
+            return self._session.fetch_my_trades(symbol, since, limit)
+        except BaseException as err:
+            print("An error occured", err)
+            exit()
