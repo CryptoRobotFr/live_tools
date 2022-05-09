e3f7a0119878666a7cb5ec8a692313cc0d36874c
TristanLeGoff <tristan.le_goff@edu.devinci.fr>
2022-05-09 15:49:01 +0200

    minor change

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ echo Installation des packages nécessaires...
 pip install -r live_tools/requirements.txt
 touch cronlog.log
 
-echo Installation finie vous pouvez maintenant commencer par modifier le fichier scret.json en ajoutant un sous compte avec ses clés api.
+echo Installation finie vous pouvez maintenant commencer par modifier le fichier secret.json en ajoutant un sous compte avec ses clés api.
