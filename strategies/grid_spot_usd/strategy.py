7640926bbc5243506cfd32ab6de7a164d721cd54
TristanLeGoff <tristan.le_goff@edu.devinci.fr>
2022-05-09 15:47:23 +0200

    refactor

--- strategies/grid_spot_usd/strategy.py
+++ strategies/grid_spot_usd/strategy.py
@@ -1,21 +1,30 @@
 import sys
-sys.path.append('./live_tools')
+
+sys.path.append("./live_tools")
 import ccxt
 import pandas as pd
 from utilities.spot_ftx import SpotFtx
 from datetime import datetime
 import time
 import json
-f = open('./live_tools/strategies/grid_spot_usd/last_data.json',)
+
+f = open(
+    "./live_tools/strategies/grid_spot_usd/last_data.json",
+)
 last_data = json.load(f)
 f.close()
-f = open('./live_tools/secret.json',)
+f = open(
+    "./live_tools/secret.json",
+)
 secret = json.load(f)
 f.close()
 
 account_to_select = "account1"
 
-def custom_grid(first_price, last_order_down = 0.5, last_order_up = 1, down_grid_len=10, up_grid_len=10):
+
+def custom_grid(
+    first_price, last_order_down=0.5, last_order_up=1, down_grid_len=10, up_grid_len=10
+):
     down_pct_unity = last_order_down / down_grid_len
     up_pct_unity = last_order_up / up_grid_len
 
@@ -23,24 +32,25 @@ def custom_grid(first_price, last_order_down = 0.5, last_order_up = 1, down_grid
     grid_buy = []
 
     for i in range(down_grid_len):
-        grid_buy.append(first_price - first_price*down_pct_unity*(i+1))
+        grid_buy.append(first_price - first_price * down_pct_unity * (i + 1))
 
     for i in range(up_grid_len):
-        grid_sell.append(first_price + first_price*up_pct_unity*(i+1))
+        grid_sell.append(first_price + first_price * up_pct_unity * (i + 1))
 
     return grid_buy, grid_sell
+
 
 now = datetime.now()
 print(now.strftime("%d-%m %H:%M:%S"))
 
 
 ftx = SpotFtx(
-        apiKey=secret[account_to_select]["apiKey"],
-        secret=secret[account_to_select]["secret"],
-        subAccountName=secret[account_to_select]["subAccountName"]
-    )
+    apiKey=secret[account_to_select]["apiKey"],
+    secret=secret[account_to_select]["secret"],
+    subAccountName=secret[account_to_select]["subAccountName"],
+)
 
-symbol = 'BTC/USD'
+symbol = "BTC/USD"
 coin1 = "BTC"
 coin2 = "USD"
 total_orders = 10
@@ -61,23 +71,47 @@ coin1_balance = ftx.get_detail_balance_of_one_coin(coin1)["free"]
 coin2_balance = ftx.get_detail_balance_of_one_coin(coin2)["free"]
 # print(coin1_balance, coin2_balance)
 
-if df_order.empty or len(df_order.loc[df_order["side"] == "buy"]) == 0 or len(df_order.loc[df_order["side"] == "sell"]) == 0:
+if (
+    df_order.empty
+    or len(df_order.loc[df_order["side"] == "buy"]) == 0
+    or len(df_order.loc[df_order["side"] == "sell"]) == 0
+):
     print("create new grid")
-    grid_buy, grid_sell = custom_grid(current_price, last_order_down = 0.4, last_order_up = 1.2, down_grid_len=5, up_grid_len=5)
+    grid_buy, grid_sell = custom_grid(
+        current_price,
+        last_order_down=0.4,
+        last_order_up=1.2,
+        down_grid_len=5,
+        up_grid_len=5,
+    )
     for buy in grid_buy:
         # print(buy,(coin2_balance/buy)/len(grid_buy))
-        ftx.place_limit_order(symbol=symbol, side='buy', amount=(coin2_balance/buy)/len(grid_buy), price=buy)
+        ftx.place_limit_order(
+            symbol=symbol,
+            side="buy",
+            amount=(coin2_balance / buy) / len(grid_buy),
+            price=buy,
+        )
 
     for sell in grid_sell:
         # print(sell,coin1_balance/len(grid_sell))
-        ftx.place_limit_order(symbol=symbol, side='sell', amount=coin1_balance/len(grid_sell), price=sell)
+        ftx.place_limit_order(
+            symbol=symbol,
+            side="sell",
+            amount=coin1_balance / len(grid_sell),
+            price=sell,
+        )
 
 elif total_orders == len(df_order):
     print("no new orders")
 
 else:
-    buy_order_to_create = last_data["number_of_sell_orders"] - len(df_order.loc[df_order["side"] == "sell"])
-    sell_order_to_create = last_data["number_of_buy_orders"] - len(df_order.loc[df_order["side"] == "buy"])
+    buy_order_to_create = last_data["number_of_sell_orders"] - len(
+        df_order.loc[df_order["side"] == "sell"]
+    )
+    sell_order_to_create = last_data["number_of_buy_orders"] - len(
+        df_order.loc[df_order["side"] == "buy"]
+    )
     print("Create", buy_order_to_create, "new buy orders")
     print("Create", sell_order_to_create, "new sell orders")
     last_buy_order = df_order.loc[df_order["side"] == "buy"]["price"].max()
@@ -88,10 +122,20 @@ else:
 
     for i in range(buy_order_to_create):
         # print("buy",current_price - diff_buy*(i+1))
-        ftx.place_limit_order(symbol=symbol, side='buy', amount=(coin2_balance/current_price)/buy_order_to_create, price=current_price - diff_buy*(i+1))
+        ftx.place_limit_order(
+            symbol=symbol,
+            side="buy",
+            amount=(coin2_balance / current_price) / buy_order_to_create,
+            price=current_price - diff_buy * (i + 1),
+        )
     for i in range(sell_order_to_create):
         # print("sell",current_price + diff_sell*(i+1))
-        ftx.place_limit_order(symbol=symbol, side='sell', amount=coin1_balance/sell_order_to_create, price=current_price + diff_sell*(i+1))
+        ftx.place_limit_order(
+            symbol=symbol,
+            side="sell",
+            amount=coin1_balance / sell_order_to_create,
+            price=current_price + diff_sell * (i + 1),
+        )
 
 orders_list = []
 for order in ftx.get_open_order():
@@ -105,5 +149,5 @@ else:
     last_data["number_of_buy_orders"] = 0
     last_data["number_of_sell_orders"] = 0
 
-with open('./live_tools/strategies/grid_spot_usd/last_data.json', 'w') as outfile:
+with open("./live_tools/strategies/grid_spot_usd/last_data.json", "w") as outfile:
     json.dump(last_data, outfile)
--- strategies/super_reversal/strategy_multi.py
+++ strategies/super_reversal/strategy_multi.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.append('./live_tools')
+
+sys.path.append("./live_tools")
 import ccxt
 import ta
 import pandas as pd
@@ -8,7 +9,10 @@ from utilities.custom_indicators import SuperTrend
 from datetime import datetime
 import time
 import json
-f = open('./live_tools/secret.json',)
+
+f = open(
+    "./live_tools/secret.json",
+)
 secret = json.load(f)
 f.close()
 
@@ -21,46 +25,46 @@ params_coin = {
         "st_short_atr_window": 15,
         "st_short_atr_multiplier": 5,
         "short_ema_window": 10,
-        "long_ema_window": 400
+        "long_ema_window": 400,
     },
     "ETH/USD": {
         "wallet_exposure": 0.3,
         "st_short_atr_window": 15,
         "st_short_atr_multiplier": 5,
         "short_ema_window": 10,
-        "long_ema_window": 400
+        "long_ema_window": 400,
     },
     "LTC/USD": {
         "wallet_exposure": 0.2,
         "st_short_atr_window": 15,
         "st_short_atr_multiplier": 5,
         "short_ema_window": 5,
-        "long_ema_window": 400
+        "long_ema_window": 400,
     },
     "SOL/USD": {
         "wallet_exposure": 0.1,
         "st_short_atr_window": 15,
         "st_short_atr_multiplier": 5,
         "short_ema_window": 40,
-        "long_ema_window": 400
+        "long_ema_window": 400,
     },
     "LUNA/USD": {
         "wallet_exposure": 0.1,
         "st_short_atr_window": 15,
         "st_short_atr_multiplier": 5,
         "short_ema_window": 15,
-        "long_ema_window": 400
+        "long_ema_window": 400,
     },
 }
 
-if sum(d['wallet_exposure'] for d in params_coin.values() if d) > 1:
+if sum(d["wallet_exposure"] for d in params_coin.values() if d) > 1:
     raise ValueError("Wallet exposure must be less or equal than 1")
 
 ftx = SpotFtx(
-        apiKey = secret[account_to_select]["apiKey"],
-        secret = secret[account_to_select]["secret"],
-        subAccountName = secret[account_to_select]["subAccountName"]
-    )
+    apiKey=secret[account_to_select]["apiKey"],
+    secret=secret[account_to_select]["secret"],
+    subAccountName=secret[account_to_select]["subAccountName"],
+)
 
 now = datetime.now()
 current_time = now.strftime("%d/%m/%Y %H:%M:%S")
@@ -71,7 +75,9 @@ open_orders = ftx.get_open_order()
 for order in open_orders:
     order = order["info"]
     if float(order["filledSize"]) > 0:
-        print(f"Order on {order['market']} is partially fill, create {order['side']} Market of {order['remainingSize']} {order['market']} order to complete it")
+        print(
+            f"Order on {order['market']} is partially fill, create {order['side']} Market of {order['remainingSize']} {order['market']} order to complete it"
+        )
         ftx.cancel_all_open_order(order["market"])
         ftx.place_market_order(order["market"], order["side"], order["remainingSize"])
 
@@ -82,24 +88,28 @@ for pair in params_coin:
     df = ftx.get_last_historical(pair, timeframe, 1000)
     # -- Populate indicators --
     super_trend = SuperTrend(
-        df['high'], 
-        df['low'], 
-        df['close'], 
-        params["st_short_atr_window"], 
-        params["st_short_atr_multiplier"]
+        df["high"],
+        df["low"],
+        df["close"],
+        params["st_short_atr_window"],
+        params["st_short_atr_multiplier"],
     )
-    
-    df['super_trend_direction'] = super_trend.super_trend_direction()
-    df['ema_short'] = ta.trend.ema_indicator(close=df['close'], window=params["short_ema_window"])
-    df['ema_long'] = ta.trend.ema_indicator(close=df['close'], window=params["long_ema_window"])
-    
+
+    df["super_trend_direction"] = super_trend.super_trend_direction()
+    df["ema_short"] = ta.trend.ema_indicator(
+        close=df["close"], window=params["short_ema_window"]
+    )
+    df["ema_long"] = ta.trend.ema_indicator(
+        close=df["close"], window=params["long_ema_window"]
+    )
+
     df_list[pair] = df
 
 coin_balance = ftx.get_all_balance()
 coin_in_usd = ftx.get_all_balance_in_usd()
-usd_balance = coin_balance['USD']
-del coin_balance['USD']
-del coin_in_usd['USD']
+usd_balance = coin_balance["USD"]
+del coin_balance["USD"]
+del coin_in_usd["USD"]
 total_balance = usd_balance + sum(coin_in_usd.values())
 available_wallet_pct = 1
 
@@ -113,28 +123,35 @@ pair_to_check = list(set(params_coin.keys()) - set(positions))
 for pair in pair_to_check:
     # iloc -2 to get the last completely close candle
     row = df_list[pair].iloc[-2]
-    if row['super_trend_direction'] == True and row['ema_short'] > row['ema_long']:
-        buy_limit_price = float(ftx.convert_price_to_precision(pair, row['ema_short']))
-        buy_quantity_in_usd = usd_balance * (params_coin[pair]["wallet_exposure"] / available_wallet_pct)
-        buy_quantity = float(ftx.convert_amount_to_precision(pair, buy_quantity_in_usd / buy_limit_price))
+    if row["super_trend_direction"] == True and row["ema_short"] > row["ema_long"]:
+        buy_limit_price = float(ftx.convert_price_to_precision(pair, row["ema_short"]))
+        buy_quantity_in_usd = usd_balance * (
+            params_coin[pair]["wallet_exposure"] / available_wallet_pct
+        )
+        buy_quantity = float(
+            ftx.convert_amount_to_precision(pair, buy_quantity_in_usd / buy_limit_price)
+        )
         exchange_buy_quantity = buy_quantity * buy_limit_price
-        print(f"Place Buy Limit Order: {buy_quantity} {pair[:-4]} at the price of {buy_limit_price}$ ~{round(exchange_buy_quantity, 2)}$")
+        print(
+            f"Place Buy Limit Order: {buy_quantity} {pair[:-4]} at the price of {buy_limit_price}$ ~{round(exchange_buy_quantity, 2)}$"
+        )
         ftx.place_limit_order(pair, "buy", buy_quantity, buy_limit_price)
-        
+
 for pair in positions:
     row = df_list[pair].iloc[-2]
-    if row['super_trend_direction'] == False or row['ema_short'] < row['ema_long']:
-        sell_limit_price = float(ftx.convert_price_to_precision(pair, row['ema_short']))
-        sell_quantity = float(ftx.convert_amount_to_precision(pair, coin_balance[pair[:-4]]))
+    if row["super_trend_direction"] == False or row["ema_short"] < row["ema_long"]:
+        sell_limit_price = float(ftx.convert_price_to_precision(pair, row["ema_short"]))
+        sell_quantity = float(
+            ftx.convert_amount_to_precision(pair, coin_balance[pair[:-4]])
+        )
         exchange_sell_quantity = sell_quantity * sell_limit_price
-        print(f"Place Sell Limit Order: {sell_quantity} {pair[:-4]} at the price of {sell_limit_price}$ ~{round(exchange_sell_quantity, 2)}$")
+        print(
+            f"Place Sell Limit Order: {sell_quantity} {pair[:-4]} at the price of {sell_limit_price}$ ~{round(exchange_sell_quantity, 2)}$"
+        )
         ftx.place_limit_order(pair, "sell", sell_quantity, sell_limit_price)
-        
+
 new_coin_in_usd = ftx.get_all_balance_in_usd()
-new_coin_in_usd = {x:y for x,y in new_coin_in_usd.items() if y!=0}
+new_coin_in_usd = {x: y for x, y in new_coin_in_usd.items() if y != 0}
 for coin in new_coin_in_usd:
-    new_coin_in_usd[coin] = str(round(new_coin_in_usd[coin], 2))+" $"
-print("My current balance in USD:",new_coin_in_usd)
-
-
-
+    new_coin_in_usd[coin] = str(round(new_coin_in_usd[coin], 2)) + " $"
+print("My current balance in USD:", new_coin_in_usd)
